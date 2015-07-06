96965dbca9512241732230e79f6f403e0c5ea8b7
Đặng Đức Vinh <ducvinh0812@gmail.com>
2015-07-06 13:54:30 +0700

    Commit

---
+++ DVChart/LineChartVC.swift
@@ -0,0 +1,77 @@
+//
+//  LineChartVC.swift
+//  DVChart
+//
+//  Created by Vinh Dang Duc on 7/6/15.
+//  Copyright © 2015 Vinh Dang Duc. All rights reserved.
+//
+
+import UIKit
+
+class LineChartVC: UIViewController {
+
+    var data: [String: Int] = [
+        "2001" : 12,
+        "2002" : 30,
+        "2003" : 25,
+        "2004" : 10,
+        "2005" : 23,
+        "2006" : 14,
+        "2007" : 24
+    ]
+    
+    weak var chart: DVChart?
+    
+    let deviceWidth = UIScreen.mainScreen().bounds.size.width
+    let deviceHeight = UIScreen.mainScreen().bounds.size.height
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Linechart"
+        
+        let myChart = DVChart(target: self, frame: CGRect(x: 0, y: 0, width: deviceWidth - 40, height: 400), type: .LineChart, data: data)
+        myChart.view.center = CGPoint(x: deviceWidth/2, y: deviceHeight/2)
+        myChart.show()
+        chart = myChart
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func touchAction(sender: AnyObject) {
+        for key in self.data.keys {
+            data[key] = Int(arc4random()%30)
+        }
+        
+        UIView.animateWithDuration(0.2, animations: {
+            self.chart?.view.alpha = 0
+            }, completion: { finished in
+                UIView.animateWithDuration(0.2, animations: {
+                    self.chart?.data = self.data
+                    self.chart?.view.alpha = 1
+                })
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
